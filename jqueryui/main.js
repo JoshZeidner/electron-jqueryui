6be261c75503631e1469cedda9542f762be093ee
Joshua Zeidner <jjzeidner@Josh-Macbook.local>
2017-10-24 08:02:32 -0400

    updated main.js

--- jqueryui/main.js
+++ jqueryui/main.js
@@ -1,8 +1,11 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
 
-require('crash-reporter').start();
-
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
 app.on('window-all-closed', function(){
